9fd4005f7668a193cd46a6a39acd3a25ec81398a
Marko Vujnovic <mv@201created.de>
2020-04-22 13:01:37 +0200

    Made the library into a Symfony bundle including all necessary service definitions

---
+++ src/C201DddBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace C201\Ddd;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Marko Vujnovic <mv@201created.de>
+ * @since  2020-04-22
+ */
+class C201DddBundle extends Bundle
+{
+}
